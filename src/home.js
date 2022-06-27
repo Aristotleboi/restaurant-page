5a0784a3e452da9e7897d150331f4e6fb5d3505d
aristotleboi <gage24paul@gmail.com>
2022-06-26 19:17:46 -0700

    first content module done:
    :

---
+++ src/home.js
@@ -0,0 +1,19 @@
+const createContent = () => {
+    const content = document.querySelector('#content');
+
+    const header = document.createElement('h1');
+    header.setAttribute('id', 'home-text')
+    header.innerText = "The Finest Instant Ramen";
+    const para = document.createElement('p')
+    para.setAttribute('id', 'home-p')
+    para.innerText = `"We're dedicated to bringing you only the finest instant ramen. Our collection had been hand curated since 2022"`;
+    const ramenImage = document.createElement('img')
+    ramenImage.setAttribute('id', 'home-image')
+    ramenImage.src = '/home/aristotleboi/repos/restaurant-page/src/ramen.jpeg';
+    ramenImage.alt = 'picture of various ramen';
+    content.appendChild(header);
+    content.appendChild(ramenImage)
+    content.appendChild(para);
+}
+
+export default createContent;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import createContent from './home.js';
+import './style.css'
+
+const header = document.querySelector('.header')
+
+createContent()
 console.log('hello')
+
